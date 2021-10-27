09ec24953ab7c2319d0e4d82827c4c60c01c4083
RumenKanev <rkynev@gmail.com>
2021-10-27 11:06:42 +0100

    test_helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
   feature 'Enter names' do
     scenario 'submitting names' do
-      visit('/')
-      fill_in :player_1_name, with: 'Tom'
-      fill_in :player_2_name, with: 'Ana'
-      click_button 'Submit'
+      sign_in_and_play
 
       # save_and_open_page # will save the web page and open the browser to display it
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'features/web_helpers'
+
 ENV['RACK_ENV'] = 'test'
 
 # require our Sinatra app file
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
-
-require 'capybara'
-require 'capybara/rspec'
-require 'rspec'
 
 # tell Capybara about our app class
 Capybara.app = Battle
